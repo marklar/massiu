09f58c787f4cf450be6d77a47b43d34ded40add2
Mark Wong-VanHaren <markwvh@gmail.com>
2013-05-27 06:34:12 +0200

    another "fix" to MongoClient connection

--- src/util/store.py
+++ src/util/store.py
@@ -20,11 +20,11 @@ db = None
 def drop_coll(collection_name):
     get_db()[collection_name].drop()
 
-def get_client(host, port):
+def get_client(loc):
     """ Cache MongoClient. """
     global client
     if client is None:
-        client = pymongo.MongoClient(host, port)
+        client = pymongo.MongoClient(loc)
     return client
 
 def get_db():
@@ -35,13 +35,20 @@ def get_db():
         mongo_url = os.getenv('MONGOHQ_URL', DEF_MONGO_URI)
         uri = urlsplit(mongo_url)
 
-        # Get your DB
-        client = get_client(uri.netloc, uri.port)
-        db = client[uri.path[1:]]
-        # authenticate
-        if '@' in mongo_url:
-            user_pass = uri.netloc.split('@')[0].split(':')
-            db.authenticate(user_pass[0], user_pass[1])
+        # Authentication necessary?
+        if '@' in uri.netloc:
+            # Get DB
+            parts = uri.netloc.split('@')
+            client = get_client(parts[1])
+            db = client[uri.path[1:]]
+            # Authenticate
+            (user, passwd) = parts[0].split(':')
+            db.authenticate(user, passwd)
+        else:
+            # Get DB
+            client = get_client(uri.netloc)
+            db = client[uri.path[1:]]
+
     return db
 
 #-- collection meta-information --
